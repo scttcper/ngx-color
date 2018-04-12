37553183389ddeb8da519509a287fc59c8a448d5
Scott Cooper <scttcper@gmail.com>
2018-04-11 20:38:46 -0700

    fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,6 +13,7 @@ describe('AppComponent', () => {
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
+    fixture.detectChanges();
     expect(app).toBeTruthy();
   }));
 });
