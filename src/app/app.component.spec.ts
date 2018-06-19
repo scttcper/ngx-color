68c51e7adaceaa531091bb1f23d67e4a21f6357c
Scott Cooper <scttcper@gmail.com>
2018-06-18 23:02:36 -0700

    chore: mock github

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,14 +1,24 @@
 import { async, TestBed } from '@angular/core/testing';
+
+import { ButtonService } from '@ctrl/ngx-github-buttons';
+import { of as ObservableOf } from 'rxjs';
+
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
+
+class FakeButtonService {
+  repo(user: string, repo: string) {
+    return ObservableOf({ stargazers_count: 0 });
+  }
+}
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [],
       imports: [AppModule],
-    });
-    TestBed.compileComponents();
+      providers: [{ provide: ButtonService, useClass: FakeButtonService }],
+    }).compileComponents();
   }));
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
