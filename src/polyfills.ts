dd78d61a419b4f09ec5055e150e310bed7fa5432
Scott Cooper <scttcper@gmail.com>
2019-01-01 08:36:53 -0800

    chore: update dep

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -39,7 +39,7 @@
  * Googlebot uses a renderer based on Chrome 41.
  * https://developers.google.com/search/docs/guides/rendering
  **/
-// import 'core-js/es6/array';
+import 'core-js/es6/array';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
