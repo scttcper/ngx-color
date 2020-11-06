c9ccda586141991197efa924510535b0d7080767
Scott Cooper <scttcper@gmail.com>
2020-11-05 22:27:39 -0800

    lint

--- src/lib/components/github/github-swatch.component.ts
+++ src/lib/components/github/github-swatch.component.ts
@@ -1,33 +1,27 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  EventEmitter,
-  Input,
-  Output,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'color-github-swatch',
   template: `
-  <div class="github-swatch">
-    <color-swatch
-      [color]="color"
-      [focusStyle]="focusStyle"
-      (onClick)="handleClick($event)"
-      (onHover)="onSwatchHover.emit($event)"
-      class="swatch"
-    ></color-swatch>
-    <div class="clear"></div>
-  </div>
+    <div class="github-swatch">
+      <color-swatch
+        [color]="color"
+        [focusStyle]="focusStyle"
+        (onClick)="handleClick($event)"
+        (onHover)="onSwatchHover.emit($event)"
+        class="swatch"
+      ></color-swatch>
+      <div class="clear"></div>
+    </div>
   `,
   styles: [
     `
-    .github-swatch {
-      width: 25px;
-      height: 25px;
-      font-size: 0;
-    }
-  `,
+      .github-swatch {
+        width: 25px;
+        height: 25px;
+        font-size: 0;
+      }
+    `,
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   preserveWhitespaces: false,
@@ -37,11 +31,11 @@ export class GithubSwatchComponent {
   @Output() onClick = new EventEmitter<any>();
   @Output() onSwatchHover = new EventEmitter<any>();
   focusStyle = {
-    'position': 'relative',
+    position: 'relative',
     'z-index': '2',
-    'outline': '2px solid #fff',
+    outline: '2px solid #fff',
     'box-shadow': '0 0 5px 2px rgba(0,0,0,0.25)',
-  }
+  };
 
   handleClick({ hex, $event }) {
     this.onClick.emit({ hex, $event });
