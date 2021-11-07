ab29ee5249c230f59960603613ed7bcfa9d15cc8
Nils Tijtgat <nils.tijtgat@showpad.com>
2021-11-07 17:24:43 +0100

    fix: Angular 13 exports (#360)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { GhButtonModule } from '@ctrl/ngx-github-buttons';
 
-import { ColorAlphaModule } from '../lib/components/alpha/alpha-picker.component';
-import { ColorBlockModule } from '../lib/components/block/block.component';
-import { ColorChromeModule } from '../lib/components/chrome/chrome.component';
-import { ColorCircleModule } from '../lib/components/circle/circle.component';
-import { ColorCompactModule } from '../lib/components/compact/compact.component';
-import { ColorGithubModule } from '../lib/components/github/github.component';
-import { ColorHueModule } from '../lib/components/hue/hue-picker.component';
-import { ColorMaterialModule } from '../lib/components/material/material.component';
-import { ColorPhotoshopModule } from '../lib/components/photoshop/photoshop.component';
-import { ColorSketchModule } from '../lib/components/sketch/sketch.component';
-import { ColorSliderModule } from '../lib/components/slider/slider.component';
-import { ColorSwatchesModule } from '../lib/components/swatches/swatches.component';
-import { ColorTwitterModule } from '../lib/components/twitter/twitter.component';
+import { ColorAlphaModule } from '../lib/alpha/alpha-picker.component';
+import { ColorBlockModule } from '../lib/block/block.component';
+import { ColorChromeModule } from '../lib/chrome/chrome.component';
+import { ColorCircleModule } from '../lib/circle/circle.component';
+import { ColorCompactModule } from '../lib/compact/compact.component';
+import { ColorGithubModule } from '../lib/github/github.component';
+import { ColorHueModule } from '../lib/hue/hue-picker.component';
+import { ColorMaterialModule } from '../lib/material/material.component';
+import { ColorPhotoshopModule } from '../lib/photoshop/photoshop.component';
+import { ColorSketchModule } from '../lib/sketch/sketch.component';
+import { ColorSliderModule } from '../lib/slider/slider.component';
+import { ColorSwatchesModule } from '../lib/swatches/swatches.component';
+import { ColorTwitterModule } from '../lib/twitter/twitter.component';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer.component';
-import { ColorShadeModule } from '../lib/components/shade/shade-picker.component';
+import { ColorShadeModule } from '../lib/shade/shade-picker.component';
 
 @NgModule({
   declarations: [AppComponent, FooterComponent],
