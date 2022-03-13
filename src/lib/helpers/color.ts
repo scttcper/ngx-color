2a7e9270aa0b01f2d53034bd6b50ae4353e7f466
krisvlad <kristiyan.vladimirov@landmark.co.uk>
2022-03-13 22:12:21 +0200

    Fix small issue with hex values missing

--- src/lib/helpers/color.ts
+++ src/lib/helpers/color.ts
@@ -33,13 +33,13 @@ export function toState(data, oldHue?: number, disableAlpha?: boolean): Color {
   const hsl = color.toHsl();
   const hsv = color.toHsv();
   const rgb = color.toRgb();
-  const hex = rgb.a === 1 ? color.toHex() : color.toHex8()
+  const hex = rgb.a === 1 ? color.toHexString() : color.toHex8String()
 
   if (hsl.s === 0) {
     hsl.h = oldHue || 0;
     hsv.h = oldHue || 0;
   }
-  const transparent = (hex === '000000' || hex === '00000000')
+  const transparent = (hex === '#000000' || hex === '#00000000')
     && rgb.a === 0;
 
   return {
